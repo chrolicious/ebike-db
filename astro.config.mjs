0c5197949b0086115b16aa06f7caadbef164cfcc
chrolicious <158811477+chrolicious@users.noreply.github.com>
2026-04-10 09:51:32 +0200

    Rebrand to CrosserGids: astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://ebike-database.com',
+  site: 'https://crossergids.nl',
   output: 'static'
 });
